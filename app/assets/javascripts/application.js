18c4b4b43dfef68cd4aadd047afb9dfcb167552c
TheNotary <no@mail.plz>
2015-07-28 14:53:18 -0500

    starting javascript and data api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery-ujs
 //= require turbolinks
 //= require_tree .
+
+var PageUi = function (){
+  this.lastData = ""
+
+  this.updateData = function() {
+    // request data from /fresh_data
+    $.ajax({url: "/fresh_data", success: function(result){
+      alert(result);
+      // $("#div1").html(result);
+    }});
+  }
+
+};
+
+
+// set 2 second timeout to pull down new data
+
+window.PageUi = new PageUi();
