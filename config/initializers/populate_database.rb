f6bd5ca5e9b73ce3be306d7303dfdb090b623bd9
TheNotary <no@mail.plz>
2015-07-31 18:50:39 -0500

    added population data so the graph looks cool

--- config/initializers/populate_database.rb
+++ config/initializers/populate_database.rb
@@ -3,3 +3,17 @@ def populate_database
     User.create(email: "admin@eff.org", password: ENV["CATS_ADMIN_PASSWORD"], admin: true)
   end
 end
+
+
+
+def populate_signatures
+  Signature.destroy_all
+
+  17.times { FactoryGirl.create(:signature, created_at: 0.days.ago) }
+  13.times { FactoryGirl.create(:signature, created_at: 1.days.ago) }
+  5.times { FactoryGirl.create(:signature, created_at: 2.days.ago) }
+  3.times { FactoryGirl.create(:signature, created_at: 3.days.ago) }
+  7.times { FactoryGirl.create(:signature, created_at: 4.days.ago) }
+  9.times { FactoryGirl.create(:signature, created_at: 5.days.ago) }
+
+end
